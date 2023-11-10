62715eccac9fa9bf0692266ad9224b71d9f7c0d4
Anas-Ibrahim-Safwat <116971895+Anas-Ibrahim-Safwat@users.noreply.github.com>
2023-11-10 16:38:49 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
-#include "BigReal.h"
 using namespace std;
+class Machine {
 
-int main() {
-    BigReal n1("53211791346713902974913.0"),n2(31649726129731636137932.0);
-    BigReal n3 = n1 - n2;
-    cout << n3;
-    return 0;
-}
+};
+
+class Memory{
+
+};
+
+class Register{
+
+};
+
+class arithmeticUnit {
+
+};
+
