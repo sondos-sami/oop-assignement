3c075310c9ec19ec54de34a3f6a349196c4cb54a
Anas-Ibrahim-Safwat <116971895+Anas-Ibrahim-Safwat@users.noreply.github.com>
2023-11-12 05:14:29 +0200

    Update main.cpp
    read and write the address in hexadecimal notation

--- main.cpp
+++ main.cpp
@@ -2,33 +2,77 @@
 using namespace std;
 
 
+string decToHex(string n){
+    string hexDeciNum;
+    int z = stoi(n);
+    int i = 0;
+    while(z != 0){
+        int temp;
+        temp = z % 16;
+
+        if(temp < 10){
+            hexDeciNum.push_back(temp + 48);
+            i++;
+        }
+        else{
+            hexDeciNum.push_back(temp + 55);
+            i++;
+        }
+        z /= 16;
+    }
+    reverse(hexDeciNum.begin() , hexDeciNum.end());
+    return hexDeciNum;
+}
+int hexToDec(string hex){
+    int len = hex.size();
+    int base = 1;
+    int dec = 0;
+    for (int i = len - 1; i >= 0 ; --i) {
+        if(hex[i] >= '0' && hex[i] <= '9'){
+            dec += (int(hex[i]) - 48) * base;
+            base = base * 16;
+        }
+        else if(hex[i] >= 'A' && hex[i] <= 'F') {
+            dec += (int(hex[i]) - 55) * base;
+            base = base * 16;
+        }
+    }
+    return dec;
+}
+
 class Memory{
 private:
     int size;
 public:
-    vector <pair<int,string>> data;
-    Memory(int memorySize) : size(memorySize) , data(memorySize , {0 , ""}){};
+    vector <pair<string ,string>> data;
+    Memory(int memorySize) : size(memorySize) , data(memorySize , {"" , ""}){};
 
-    string read(int address){
-        if (address >= 0 && address < size){
-            return data[address].second;
+    string read(string address){
+        int n = hexToDec(address);
+        if (n >= 0 && n < size){
+            return data[n].second;
         }
         else{
             cerr << "Error: Attempt to read invalid memory address " << address << endl;
             exit(1);
         }
     }
-    void write(int address , string &value){
-        if (address >= 0 && address < size){
-          
+    void write(string address , string &value){
+        int n = stoi(address);
+        if (n >= 0 && n < size){
             string x = value.substr(0,2);
-            data[address].second =x ;
-            data[address].first = address;
-          
+            data[n].second = x;
+            if(n == 0){
+                data[n].first = "0";
+            }
+            else{
+                data[n].first = decToHex(address);
+            }
+
             x = value.substr(2 , 4);
-            data[address + 1].second = x;
-            data[address + 1].first = address + 1;
-          
+            data[n + 1].second = x;
+            data[n + 1].first = decToHex(to_string(n + 1));
+
         } else{
             cerr << "Error: Attempt to read invalid memory address " << address << endl;
             exit(1);
