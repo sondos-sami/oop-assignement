6932ddac82a0034abf2ce6bc2fbd35af8cde4cef
mariameid210 <147279883+mariameid210@users.noreply.github.com>
2023-11-14 01:06:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -119,54 +119,90 @@ public:
         return instruction;
     }
 
-    void execute(string instruction) {
+   void decode(string instruction) {
+      if (instruction[0] == '1') {//1056
+           op_code = '1';
+            output_register = instruction[1];
+           memory_cell = instruction.substr(2,4);
 
-        if (instruction[0] == '1') { // done
-            registers[instruction[1] - '0'] = memory.read(instruction.substr(2, 4));
+        } else if (instruction[0] == '2') {
+           op_code = '2';
+           output_register = instruction[1];
+           memory_val=instruction.substr(2,4);
         }
-        else if (instruction[0] == '2') { // done
-            registers[instruction[1] - '0'] = instruction.substr(2, 4);
-        }
-        else if (instruction[0] == '3') { // done
 
-            // represent the address in decimal to find its memory cell easy
-            int x = hexToDec(instruction.substr(2, 4));
-            memory.data[x].second = decToHex(registers[instruction[1] - '0']);
+      else if (instruction[0] == '3') {
+          op_code = '3';
+          output_register = instruction[1];
+         memory_cell= instruction.substr(2,4);
+      }
+      else if (instruction[0] == '4') {
+          op_code = '4';
+          output_register = instruction[3];
+          other_register = instruction[2];
+      }
+      else if (instruction[0] == '5') {
+          op_code = '5';
+          output_register = instruction[1];
+          temp_register=instruction[2];
+          other_register=instruction[3];
+      }
+      else if (instruction[0] == 'B') {
+          op_code = 'B';
+          other_register = instruction[1];
+          memory_cell= instruction.substr(2, 4);
+      }
+
+
+   }
+    void execute(string instruction){
+
+        if(op_code =='1'){
+            registers[output_register- '0'] = memory.read(memory_cell);
         }
-        else if (instruction[0] == '4') { //done
+
+        else if(op_code == '2'){
+            registers[output_register - '0'] = memory_val;
+        }
+        else if(op_code == '3'){//3043
+            int x=hexToDec(memory_cell);
+            memory.data[x].second=decToHex(registers[output_register-'0']);
+        }
+        else if (op_code == '4') { //done
             string x;
-            x.push_back(instruction[2]);
+            x.push_back(other_register);
             int x1 = hexToDec(x);
 
             string y;
-            y.push_back(instruction[3]);
+            y.push_back(output_register);
             int y1 = hexToDec(y);
 
             registers[y1] = registers[x1];
         }
-        else if (instruction[0] == '5') { //done
+        else if (op_code == '5') { //done
 
             string x;
-            x.push_back(instruction[2]);
+            x.push_back(temp_register);
             int r1 = hexToDec(x);
 
             string y;
-            y.push_back(instruction[3]);
+            y.push_back(other_register);
             int r2 = hexToDec(y);
 
             int sum = r1 + r2;
 
-            registers[instruction[1] - '0'] = to_string(sum);
+            registers[output_register - '0'] = to_string(sum);
 
         }
         else if (instruction[0] == 'B') { //done
-            if (registers[instruction[1] - '0'] == registers[0]) {
-                pc = instruction.substr(2, 4);
+            if (registers[other_register - '0'] == registers[0]) {
+                pc = memory_cell;
             }
+
         }
+
     }
-    
-       void run() {
+    void run() {
         fetch();
         decode(instruction);
         execute(instruction);
@@ -174,6 +210,32 @@ public:
           l++;
           pc=to_string(l);
     }
+};
 
+class Register:public Machine{
+    vector<string> registers;
+public:
+
+
+
+void set_value(int reg_num,string new_val){
+    registers[reg_num-1]=new_val;
+}
+void display_status(int num){//5
+cout<<"Register "<<num<<" now contains :"<<registers[num-1]<<endl;
+}
+void print_registers(){
+    for(int i=0;i<16;++i){
+        cout<<"register "<<i+1<<" contains :"<<registers[i]<<endl;
+    }
+}
 
 };
+
+
+
+
+int main(){
+
+
+}
