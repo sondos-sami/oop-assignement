cd18a30765ec0bbc1ba9dc896a964e27686ccec8
mariameid210 <147279883+mariameid210@users.noreply.github.com>
2023-11-14 01:15:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 string decToHex(string n){
-
     string hexDeciNum;
     int z = stoi(n);
     int i = 0;
-
     while(z != 0){
-
         int temp;
         temp = z % 16;
 
@@ -21,37 +19,27 @@ string decToHex(string n){
             hexDeciNum.push_back(temp + 55);
             i++;
         }
-
         z /= 16;
     }
-
     reverse(hexDeciNum.begin() , hexDeciNum.end());
     return hexDeciNum;
 }
-
 int hexToDec(string hex){
-
     int len = hex.size();
     int base = 1;
     int dec = 0;
-
     for (int i = len - 1; i >= 0 ; --i) {
-
         if(hex[i] >= '0' && hex[i] <= '9'){
             dec += (int(hex[i]) - 48) * base;
             base = base * 16;
         }
-
         else if(hex[i] >= 'A' && hex[i] <= 'F') {
             dec += (int(hex[i]) - 55) * base;
             base = base * 16;
         }
-
     }
-
     return dec;
 }
-
 class Memory{
 private:
     int size;
@@ -60,31 +48,23 @@ public:
     Memory(int memorySize) : size(memorySize) , data(memorySize , {"0" , "0"}){};
 
     string read(string address){
-
         int n = hexToDec(address);
         if (n >= 0 && n < size){
             return data[n].second;
         }
-
         else{
             cerr << "Error: Attempt to read invalid memory address " << address << endl;
             exit(1);
         }
     }
-
-    void write(string address , string &value){
-
-        int n = stoi(address);
-
+    void write(string address , string &value){//A5 ,1056
+        int n = stoi(address);//55
         if (n >= 0 && n < size){
-
-            string x = value.substr(0,2);
+            string x = value.substr(0,2);//x=10
             data[n].second = x;
-
             if(n == 0){
                 data[n].first = "0";
             }
-
             else{
                 data[n].first = decToHex(address);
             }
@@ -93,13 +73,12 @@ public:
             data[n + 1].second = x;
             data[n + 1].first = decToHex(to_string(n + 1));
 
-        }
-
-        else{
+        } else{
             cerr << "Error: Attempt to read invalid memory address " << address << endl;
             exit(1);
         }
     }
+
 };
 
 class Machine {
@@ -107,11 +86,19 @@ private:
     Memory &memory;
     string pc;
     vector<string> registers;
+    char op_code;
+    char output_register;
+    char other_register;
+    char temp_register;
+    string memory_cell;
+    string memory_val;
+    string instruction;
 public:
     Machine(Memory &mem) : memory(mem), registers(16, "0"), pc("0") {}
 
     string fetch() {
-        string instruction = memory.read(pc);
+
+        instruction= memory.read(pc);
         int n = stoi(pc);
         n++;
         pc = to_string(n);
@@ -119,42 +106,42 @@ public:
         return instruction;
     }
 
-   void decode(string instruction) {
-      if (instruction[0] == '1') {//1056
-           op_code = '1';
+    void decode(string instruction) {
+        if (instruction[0] == '1') {//1056
+            op_code = '1';
             output_register = instruction[1];
-           memory_cell = instruction.substr(2,4);
+            memory_cell = instruction.substr(2,4);
 
         } else if (instruction[0] == '2') {
-           op_code = '2';
-           output_register = instruction[1];
-           memory_val=instruction.substr(2,4);
+            op_code = '2';
+            output_register = instruction[1];
+            memory_val=instruction.substr(2,4);
         }
 
-      else if (instruction[0] == '3') {
-          op_code = '3';
-          output_register = instruction[1];
-         memory_cell= instruction.substr(2,4);
-      }
-      else if (instruction[0] == '4') {
-          op_code = '4';
-          output_register = instruction[3];
-          other_register = instruction[2];
-      }
-      else if (instruction[0] == '5') {
-          op_code = '5';
-          output_register = instruction[1];
-          temp_register=instruction[2];
-          other_register=instruction[3];
-      }
-      else if (instruction[0] == 'B') {
-          op_code = 'B';
-          other_register = instruction[1];
-          memory_cell= instruction.substr(2, 4);
-      }
+        else if (instruction[0] == '3') {
+            op_code = '3';
+            output_register = instruction[1];
+            memory_cell= instruction.substr(2,4);
+        }
+        else if (instruction[0] == '4') {
+            op_code = '4';
+            output_register = instruction[3];
+            other_register = instruction[2];
+        }
+        else if (instruction[0] == '5') {
+            op_code = '5';
+            output_register = instruction[1];
+            temp_register=instruction[2];
+            other_register=instruction[3];
+        }
+        else if (instruction[0] == 'B') {
+            op_code = 'B';
+            other_register = instruction[1];
+            memory_cell= instruction.substr(2, 4);
+        }
 
 
-   }
+    }
     void execute(string instruction){
 
         if(op_code =='1'){
@@ -207,8 +194,8 @@ public:
         decode(instruction);
         execute(instruction);
         int l=stoi(pc);
-          l++;
-          pc=to_string(l);
+        l++;
+        pc=to_string(l);
     }
 };
 
@@ -218,24 +205,24 @@ public:
 
 
 
-void set_value(int reg_num,string new_val){
-    registers[reg_num-1]=new_val;
-}
-void display_status(int num){//5
-cout<<"Register "<<num<<" now contains :"<<registers[num-1]<<endl;
-}
-void print_registers(){
-    for(int i=0;i<16;++i){
-        cout<<"register "<<i+1<<" contains :"<<registers[i]<<endl;
+    void set_value(int reg_num,string new_val){
+        registers[reg_num-1]=new_val;
     }
-}
+    void display_status(int num){//5
+        cout<<"Register "<<num<<" now contains :"<<registers[num-1]<<endl;
+    }
+    void print_registers(){
+        for(int i=0;i<16;++i){
+            cout<<"register "<<i+1<<" contains :"<<registers[i]<<endl;
+        }
+    }
 
 };
 
 
 
 
-int main(){
-
-
+int main() {
 }
+
+
