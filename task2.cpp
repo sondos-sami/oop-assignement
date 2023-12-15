b4798191074560d5b46fe1824df777ca031da8b2
Anas-Ibrahim-Safwat <116971895+Anas-Ibrahim-Safwat@users.noreply.github.com>
2023-12-15 23:37:46 +0200

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -51,6 +51,64 @@ void GameManager::run() {
 }
 
 
+//-------------------------------------------------\\
+
+X_O_Board::X_O_Board () {
+    n_rows = n_cols = 3;
+    board = new char*[n_rows];
+    for (int i = 0; i < n_rows; i++) {
+        board [i] = new char[n_cols];
+        for (int j = 0; j < n_cols; j++)
+            board[i][j] = 0;
+    }
+}
+bool X_O_Board::update_board (int x, int y, char mark){
+    // Only update if move is valid
+    if (!(x < 0 || x > 2 || y < 0 || y > 2) && (board[x][y] == 0)) {
+        board[x][y] = toupper(mark);
+        n_moves++;
+        return true;
+    }
+    else
+        return false;
+}
+void X_O_Board::display_board() {
+    for (int i: {0,1,2}) {
+        cout << "\n| ";
+        for (int j: {0,1,2}) {
+            cout << "(" << i << "," << j << ")";
+            cout << setw(2) << board [i][j] << " |";
+        }
+        cout << "\n-----------------------------";
+    }
+    cout << endl;
+}
+bool X_O_Board::is_winner() {
+    char row_win[3], col_win[3], diag_win[2];
+    for (int i:{0,1,2}) {
+        row_win[i] = board[i][0] & board[i][1] & board[i][2];
+        col_win[i] = board[0][i] & board[1][i] & board[2][i];
+    }
+    diag_win[0] = board[0][0] & board[1][1] & board[2][2];
+    diag_win[1] = board[2][0] & board[1][1] & board[0][2];
+
+    for (int i:{0,1,2}) {
+        if ( (row_win[i] && (row_win[i] == board[i][0])) ||
+             (col_win[i] && (col_win[i] == board[0][i])) )
+        {return true;}
+    }
+    if ((diag_win[0] && diag_win[0] == board[1][1]) ||
+        (diag_win[1] && diag_win[1] == board[1][1]))
+    {return true;}
+    return false;
+}
+bool X_O_Board::is_draw() {
+    return (n_moves == 9 && !is_winner());
+}
+bool X_O_Board::game_is_over () {
+    return n_moves >= 9;
+}
+
 
 //-------------------------------------------------\\-
 
