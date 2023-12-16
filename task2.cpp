58df5a45fb7a7125523f91e3a0d35569a182bcf1
Anas-Ibrahim-Safwat <116971895+Anas-Ibrahim-Safwat@users.noreply.github.com>
2023-12-16 05:43:21 +0200

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -8,6 +8,7 @@ GameManager::GameManager(Board* bPtr, Player* playerPtr[2] ) {
     players[0] = playerPtr[0];
     players[1] = playerPtr[1];
 }
+
 void GameManager::run() {
     int x, y;
 
@@ -21,35 +22,19 @@ void GameManager::run() {
             }
             boardPtr->display_board();
 
-            if (boardPtr->is_draw()){
-                cout << "Draw!\n";
-                return;
-            }
-
             if (boardPtr->is_winner()){
                 cout  << players[i]->to_string() << " wins\n";
                 return;
             }
 
+            if (boardPtr->is_draw()){
+                cout << "Draw!\n";
+                return;
+            }
+
         }
     }
-
-    if(boardPtr->is_draw()){
-        cout <<"Draw!" << endl;
-        return;
-    }
-
-    else if(boardPtr ->is_winner()){
-        cout << players[0]->to_string() << " wins!" <<endl;
-        return;
-    }
-
-    else if(!boardPtr ->is_winner()){
-        cout << players[1]->to_string() << " wins!" <<endl;
-        return;
-    }
 }
-
 
 //-------------------------------------------------\\
 
@@ -109,6 +94,78 @@ bool X_O_Board::game_is_over () {
     return n_moves >= 9;
 }
 
+//-------------------------------------------------\\
+
+Pyramic_X_O_Board::Pyramic_X_O_Board() {
+    n_rows =  3;
+    n_cols = 5;
+    board = new char*[n_rows];
+    for (int i = 0; i < n_rows; i++) {
+        board [i] = new char[n_cols];
+        for (int j = 0; j < n_cols; j++)
+            board[i][j] = 0;
+    }
+}
+bool Pyramic_X_O_Board :: update_board (int x, int y, char mark){
+    if ( !((x < 0 || x > 2 || y < 0 || y > 4) || ( (x == 0 && y == 0) || (x == 0 && y == 1) || (x == 0 && y == 3) || (x == 0 && y == 4 ) || (x == 1 && y == 0) || (x == 1 && y == 4))) && (board[x][y] == 0) ) {
+        board[x][y] = toupper(mark);
+        n_moves++;
+        return true;
+
+    }
+    else{
+        return false;
+    }
+
+}
+void Pyramic_X_O_Board :: display_board(){
+    for (int i: {0,1,2}) {
+        cout << "\n ";
+        for (int j: {0,1,2,3,4}) {
+            if(!((i == 0 && j == 0) || (i == 1 && j == 0) ||(i == 0 && j == 1) ||(i == 0 && j == 4) || (i == 1 && j == 4) ||(i == 0 && j == 3))){
+                if(i == 0){cout << "                   ";}
+                if(i == 1){cout << "     ";}
+                if(i == 2){cout << "  ";}
+                cout << "(" << i << "," << j << ")";
+                cout << setw(2)<< board [i][j] << " |";
+            }
+        }
+        cout << "\n-----------------------------------------------------------";
+    }
+    cout << endl;
+}
+bool Pyramic_X_O_Board :: is_winner(){
+    if(((board[2][0] == board[2][1]) && ((board[2][1] == board[2][2]))) && (board[2][0] == 'X' || board[2][0] == 'O') ){
+        return true;
+    }
+    else if(((board[2][1] == board[2][2]) && ((board[2][2] == board[2][3]))) && (board[2][1] == 'X' || board[2][1] == 'O')){
+        return true;
+    }
+    else if(((board[2][2] == board[2][3]) && ((board[2][3] == board[2][4]))) && (board[2][2] == 'X' || board[2][2] == 'O')){
+        return true;
+    }
+    else if(((board[1][1] == board[1][2]) && ((board[1][2] == board[1][3]))) && (board[1][1] == 'X' || board[1][1] == 'O')){
+        return true;
+    }
+    else if(((board[0][2] == board[1][2]) && ((board[1][2] == board[2][2]))) && (board[0][2] == 'X' || board[0][2] == 'O') ){
+        return true;
+    }
+    else if(((board[2][0] == board[1][1]) && ((board[1][1] == board[0][2]))) && (board[2][0] == 'X' || board[2][0] == 'O') ){
+        return true;
+    }
+    else if(((board[2][4] == board[1][3]) && ((board[1][3] == board[0][2]))) && (board[2][4] == 'X' || board[2][4] == 'O') ){
+        return true;
+    }
+    else
+        return false;
+
+}
+bool Pyramic_X_O_Board :: is_draw(){
+    return (n_moves == 9 && !is_winner());
+}
+bool Pyramic_X_O_Board :: game_is_over(){
+    return n_moves >= 9;
+}
 
 //-------------------------------------------------\\-
 
@@ -150,9 +207,9 @@ void Four_X_O_Board ::display_board() {
     cout << endl;
 }
 bool Four_X_O_Board ::is_winner() {
-    
+
     int x_count = 0, o_count = 0;
-    
+
     for (int i = 0; i < 6 ; ++i) {
         for (int j = 0; j < 4 ; ++j) {
             if( ( board[i][j] == board[i][j+1]) && (board[i][j+1] == board[i][j+2] && board[i][j+2] == board[i][j+3] ) && (board[i][j] == 'X' || board[i][j] =='O') ){
@@ -185,7 +242,7 @@ bool Four_X_O_Board ::is_winner() {
             }
         }
     }
-    
+
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 4; ++j) {
             x_count = 0;
@@ -212,6 +269,125 @@ bool Four_X_O_Board ::game_is_over() {
     return n_moves >= 42;
 }
 
+//-------------------------------------------------\\
+
+game3_Board::game3_Board () {
+    n_rows = n_cols = 5;
+    board = new char*[n_rows];
+    for (int i = 0; i < n_rows; i++) {
+        board [i] = new char[n_cols];
+        for (int j = 0; j < n_cols; j++)
+            board[i][j] = 0;
+    }
+}
+bool game3_Board::update_board (int x, int y, char mark){
+    if (!(x < 0 || x > 4 || y < 0 || y > 4) && (board[x][y] == 0)) {
+        board[x][y] = toupper(mark);
+        n_moves++;
+        return true;
+    }
+    else
+        return false;
+}
+void game3_Board::display_board() {
+    for (int i: {0,1,2,3,4}) {
+        cout << "\n| ";
+        for (int j: {0,1,2,3,4}) {
+            cout << "(" << i << "," << j << ")";
+            cout << setw(2) << board [i][j] << " | ";
+        }
+        cout << "\n--------------------------------------------------";
+    }
+    cout << endl;
+}
+bool game3_Board ::is_winner() {
+
+    int x_count = 0 , o_count =0;
+
+    for (int i = 0; i < 5 ; ++i) {
+        for (int j = 0; j < 3 ; ++j) {
+            if( (board[i][j] == board[i][j+1]) && (board[i][j+1] == board[i][j+2]) && (board[i][j] == 'X') ){
+                x_count++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5 ; ++i) {
+        for (int j = 0; j < 3 ; ++j) {
+            if( (board[j][i] == board[j+1][i]) && (board[j+1][i] == board[j+2][i]) && (board[j][i] == 'X') ){
+                x_count++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3 ; ++i) {
+        for (int j = 0; j < 3 ; ++j) {
+
+            if((board[i][j] == board[i+1][j+1]) && (board[i+1][j+1] == board[i+2][j+2]) && (board[i][j] == 'X')){
+                x_count++;
+            }
+
+            if((board[i][j+2] == board[i+1][j+1]) && (board[i+1][j+1] == board[i+2][j]) && (board[i][j+2] == 'X')){
+                x_count++;
+            }
+
+        }
+    }
+
+    //------------------------------\\
+
+    for (int i = 0; i < 5 ; ++i) {
+        for (int j = 0; j < 3 ; ++j) {
+            if( (board[i][j] == board[i][j+1]) && (board[i][j+1] == board[i][j+2]) && (board[i][j] == 'O') ){
+                o_count++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5 ; ++i) {
+        for (int j = 0; j < 3 ; ++j) {
+            if( (board[j][i] == board[j+1][i]) && (board[j+1][i] == board[j+2][i]) && (board[j][i] == 'O') ){
+                o_count++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3 ; ++i) {
+        for (int j = 0; j < 3 ; ++j) {
+            if((board[i][j] == board[i+1][j+1]) && (board[i+1][j+1] == board[i+2][j+2]) && (board[i][j] == 'O')){
+                o_count++;
+            }
+            if((board[i][j+2] == board[i+1][j+1]) && (board[i+1][j+1] == board[i+2][j]) && (board[i][j+2] == 'O')){
+                o_count++;
+            }
+
+        }
+    }
+
+
+    //--------------------------------\\
+
+    X = x_count;
+    O = o_count;
+
+    if(n_moves == 24){
+        if( X > O){
+            cout << "X - Wins!" << endl;
+        }
+        else if(X < O){
+            cout << "O - Wins!" << endl;
+        }
+    }
+   return false;
+
+}
+bool game3_Board ::is_draw() {
+    return (n_moves == 24 && X == O);
+}
+bool game3_Board ::game_is_over() {
+    return n_moves == 24;
+}
+
 
 //---------------------------------------\\
 
@@ -219,8 +395,6 @@ Player::Player(char symbol) {
     this->symbol = symbol;
 }
 Player::Player (int order, char symbol) {
-    cout << "Welcome player " << order << endl;
-    cout << "Please enter your name: ";
     cin >> name;
     this->symbol = symbol;
 }
@@ -240,7 +414,7 @@ char Player::get_symbol() {
 RandomPlayer::RandomPlayer (char symbol, int dimension):Player(symbol)
 {
     this->dimension = dimension;
-    this->name = "Random Computer Player";
+    this->name = "Computer";
     cout << "My names is " << name << endl;
 }
 void RandomPlayer::get_move (int& x, int& y) {
